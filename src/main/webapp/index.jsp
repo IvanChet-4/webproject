a4586dc6fc3fbf3f5450fccd1bcf766b06078095
IvanChet-4 <wokewylf-111@mail.ru>
2022-06-11 12:46:33 +0300

    Initial commit
    Web проект, выполняемый с 19.05.2022.

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,34 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: MTSUser
+  Date: 21.05.2022
+  Time: 13:59
+  To change this template use File | Settings | File Templates.
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Title page</title>
+    <link rel="stylesheet" href="/resourse/css/style.css">
+</head>
+
+<body>
+<div>
+    <h1>Система управления студентами и их успеваемостью</h1>
+    <a href="">Logout</a>
+</div>
+
+<div>
+    <a href="/students">Студенты</a>
+    <a href="">Дисциплины</a>
+    <a href="">Семестры</a>
+</div>
+
+</body>
+</html>
---
+++ src/main/webapp/resourse/js/functions.js
@@ -0,0 +1,38 @@
+function deleteStudents() {
+
+    var checkedCheckboxs = document.querySelectorAll('input[name=idStudent]:checked')
+
+    if (checkedCheckboxs.length == 0){
+        alert("Пожалуйста, выберите хотябы одного студента...");
+        return;
+    }
+
+    var ids = "";
+
+    for (var i = 0; i < checkedCheckboxs.length; i++){
+        ids = ids + checkedCheckboxs[i].value + " "
+
+    }
+    document.getElementById("idsHiddenDelete").value = ids;
+    document.getElementById("deleteForm").submit();
+}
+
+function modifyStudent() {
+
+    var checkedCheckboxs = document.querySelectorAll('input[name=idStudent]:checked')
+
+    if (checkedCheckboxs.length == 0){
+        alert("Пожалуйста, выберите одного студента...");
+        return;
+    }
+
+    if (checkedCheckboxs.length > 1){
+        alert("Выберите только одного студента.");
+        return;
+    }
+
+    var id = checkedCheckboxs[0].value;
+
+    document.getElementById("idHiddenModify").value = id;
+    document.getElementById("modifyForm").submit();
+}
