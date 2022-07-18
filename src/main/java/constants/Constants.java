7dcf5370fa669c240838eb12006f973690fe6ab3
IvanChet-4 <wokewylf-111@mail.ru>
2022-07-18 23:05:53 +0300

    Добавлены: Создание дисиципины, Удаление дисциплины, Модификация дисциплины, Фильтр логин

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -6,5 +6,5 @@ public interface Constants {
     //LOCAL
     String CONNECTION_URL = "jdbc:mysql://localhost:3306/students_28_29?user=root&password=Gateway14";
     //REMOTE
-   // String CONNECTION_URL = "jdbc:mysql://localhost:3306/students_28_29?user=root19&password=root19011994";
+    //String CONNECTION_URL = "jdbc:mysql://localhost:3306/students_28_29?user=root19&password=root19011994";
 }
